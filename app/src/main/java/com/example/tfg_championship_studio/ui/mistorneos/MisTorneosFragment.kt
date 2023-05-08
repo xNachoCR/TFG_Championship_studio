ef813b7a3d6f7c5412bb9b6aedf81161795ed72b
xNachoCR <nacho.crubio@gmail.com>
2023-05-08 22:47:23 +0200

    Guardado previo a los cambios a la hora de pedir datos para hacer el torneo

--- app/src/main/java/com/example/tfg_championship_studio/ui/mistorneos/MisTorneosFragment.kt
+++ app/src/main/java/com/example/tfg_championship_studio/ui/mistorneos/MisTorneosFragment.kt
@@ -1,15 +1,18 @@
 package com.example.tfg_championship_studio.ui.mistorneos
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tfg_championship_studio.R
 import com.example.tfg_championship_studio.adapter_torneos.TorneosAdapter
 import com.example.tfg_championship_studio.databinding.FragmentMisTorneosBinding
@@ -46,13 +49,13 @@ class MisTorneosFragment : Fragment() {
 
 
     private fun initRecyclerView(listaTorneos: MutableList<Torneos>) {
-        val manager = LinearLayoutManager(context)
-        val decoration = DividerItemDecoration(context, manager.orientation)
+        var manager = LinearLayoutManager(context)
+        manager.orientation = LinearLayoutManager.VERTICAL
+        var decoration = DividerItemDecoration(context, manager.orientation)
         addTorneo(listaTorneos)
         binding.rvTorneos.layoutManager = manager
         binding.rvTorneos.adapter = TorneosAdapter(listaTorneos)
         binding.rvTorneos.addItemDecoration(decoration)
-
     }
 
     private fun addTorneo(torneosList: MutableList<Torneos>){
@@ -72,62 +75,9 @@ class MisTorneosFragment : Fragment() {
             modelPlayer = ModelPlayer.SINGLES
         )
 
-        val torneo3 = Torneos(
-            icon = R.drawable.google.toString(),
-            name = "Prueba",
-            nComp = 8,
-            tournament = TournamentType.LEAGUE,
-            modelPlayer = ModelPlayer.SINGLES
-        )
-
-        val torneo4 = Torneos(
-            icon = R.drawable.google.toString(),
-            name = "Hola que tal",
-            nComp = 10,
-            tournament = TournamentType.LEAGUE,
-            modelPlayer = ModelPlayer.SINGLES
-        )
-
-        val torneo5 = Torneos(
-            icon = R.drawable.google.toString(),
-            name = "Prueba",
-            nComp = 8,
-            tournament = TournamentType.LEAGUE,
-            modelPlayer = ModelPlayer.SINGLES
-        )
-
-        val torneo6 = Torneos(
-            icon = R.drawable.google.toString(),
-            name = "Hola que tal",
-            nComp = 10,
-            tournament = TournamentType.LEAGUE,
-            modelPlayer = ModelPlayer.SINGLES
-        )
-
-        val torneo7 = Torneos(
-            icon = R.drawable.google.toString(),
-            name = "Prueba",
-            nComp = 8,
-            tournament = TournamentType.LEAGUE,
-            modelPlayer = ModelPlayer.SINGLES
-        )
-
-        val torneo8 = Torneos(
-            icon = R.drawable.google.toString(),
-            name = "Hola que tal",
-            nComp = 10,
-            tournament = TournamentType.LEAGUE,
-            modelPlayer = ModelPlayer.SINGLES
-        )
-
         torneosList.add(torneo)
         torneosList.add(torneo2)
-        torneosList.add(torneo3)
-        torneosList.add(torneo4)
-        torneosList.add(torneo5)
-        torneosList.add(torneo6)
-        torneosList.add(torneo7)
-        torneosList.add(torneo8)
+
     }
 
     override fun onDestroyView() {
